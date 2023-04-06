f0f0c2c8743aced13974aaeefbfd651b286aa7f5
Iri <129388241+irinush24@users.noreply.github.com>
2023-04-06 20:55:12 +0300

    Add files via upload
    Lipseste tot folderul de node_modules, nu am rabdare sa incarc asa ceva

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily:{raleway:"'Raleway', sans-serif;"}
+  },
+  plugins: [],
+}
